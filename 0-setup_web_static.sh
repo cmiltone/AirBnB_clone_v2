c71b47d3df1d1912619e64bc81270d034cd9d42a
Castro Hamiltone <castromiltone@gmail.com>
2023-09-06 22:54:44 +0300

    fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -26,15 +26,26 @@ chgrp -R ubuntu /data/
 ln -s /data/web_static/releases/test/ /data/web_static/current
 
 printf %s "server {
-     listen      80 default_server;
-     listen      [::]:80 default_server;
-     root        /etc/nginx/html;
-     index       index.html index.htm;
+    listen      80 default_server;
+    listen      [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
 
-     location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-     }
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+
+    location /hbnb_static {
+      alias /data/web_static/current;
+      index index.html index.htm;
+    }
 }
 " > /etc/nginx/sites-available/default
 service nginx restart
