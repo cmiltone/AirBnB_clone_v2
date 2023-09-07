6cdce6eb22f3a8dac78c988b2c4987e3a9ea2698
Castro Hamiltone <castromiltone@gmail.com>
2023-09-07 21:17:50 +0300

    fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,37 +1,31 @@
 #!/usr/bin/env bash
 # script sets up your web servers for the deployment of web_static
 
+#!/usr/bin/env bash
+# Sets up a web server for deployment of web_static.
+
 apt-get update
 apt-get install -y nginx
 
-mkdir -p /data/web_static/shared
-mkdir -p /data/web_static/releases/test
-
-printf %s "<html>
-  <head>
-  </head>
-  <body>
-    ALX School
-  </body>
-</html>
-" > /data/web_static/releases/test/index.html
-
-mydir='/data/web_static/current'
-if [ -L $mydir ]; then
-  rm -R '/data/web_static/current'
-fi
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Hello World!" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 chown -R ubuntu /data/
 chgrp -R ubuntu /data/
 
-ln -s /data/web_static/releases/test/ /data/web_static/current
-
 printf %s "server {
-    listen      80 default_server;
-    listen      [::]:80 default_server;
+    listen 80 default_server;
+    listen [::]:80 default_server;
     add_header X-Served-By $HOSTNAME;
     root   /var/www/html;
     index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
 
     location /redirect_me {
         return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
@@ -42,11 +36,6 @@ printf %s "server {
       root /var/www/html;
       internal;
     }
+}" > /etc/nginx/sites-available/default
 
-    location /hbnb_static {
-      alias /data/web_static/current;
-      index index.html index.htm;
-    }
-}
-" > /etc/nginx/sites-available/default
 service nginx restart
