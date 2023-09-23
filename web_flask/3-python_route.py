da4a0629059d1e9b947ca2f84566e76ecbec954a
Castro Hamiltone <castromiltone@gmail.com>
2023-09-23 14:48:25 +0300

    3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""starts flask server"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """route shows Hello HBNB"""
+    return "Hello HBNB"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """route shows HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    _str = str(text).replace('_', ' ')
+    return "C " + _str
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    _str = str(text).replace('_', ' ')
+    return "Python " + _str
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
