8720adc01d307fc4ed69b1b14ebe22903fed1c0b
Castro Hamiltone <castromiltone@gmail.com>
2023-09-23 14:03:22 +0300

    fix

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,7 +8,8 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_world():
+    """route shows Hello HBNB"""
     return "Hello HBNB"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0")
