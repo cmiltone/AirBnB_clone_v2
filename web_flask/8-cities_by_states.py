34e19c066db3bf210c36921bcce3289f8f1319a1
Castro Hamiltone <castromiltone@gmail.com>
2023-09-23 16:32:15 +0300

    web_flask/8-cities_by_states.py, web_flask/templates/8-cities_by_states.html

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""starts flask server"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """cities_by_states route"""
+    all_states = storage.all(State)
+    return render_template('web_flask/templates/8-cities_by_states.html', states=all_states)
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    """Remove current session."""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
