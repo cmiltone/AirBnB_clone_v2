a4414a09c4d7e0f17ba648ad43e7795aff785307
Castro Hamiltone <castromiltone@gmail.com>
2023-09-23 14:21:27 +0300

    web_flask/1-hbnb_route.py

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""starts flask server"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """route shows Hello HBNB"""
+    return "Hello HBNB"
+
+
+def bnb():
+    """route shows HBNB"""
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
